9c4fe4178364f88dd4f6316a94c3555c70947b10
Trey Deitch <me@tdeitch.com>
2019-07-13 18:32:15 -0400

    Add slime to emacs

--- dotfiles/.emacs.d/init.el
+++ dotfiles/.emacs.d/init.el
@@ -44,3 +44,19 @@
   "Default Python interpreter for shell."
   :type 'string
   :group 'python)
+
+; Use sbcl with slime
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (slime))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
